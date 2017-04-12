8b05ff9bfd228bcc755b708878ad8ae7a963b58f
Moses Franco <mosesf@outlook.com>
2017-04-12 15:49:37 -0500

    Updated using for and for/each loops

--- js/iterating.js
+++ js/iterating.js
@@ -6,8 +6,21 @@
     // TODO: Create a log statement that will log the number of elements in the names array.
     console.log(names.length);
     // TODO: Create log statements that will print each of the names array elements individually.
-    console.log(names[0]);
-    console.log(names[1]);
-    console.log(names[2]);
-    console.log(names[3]);
+    for (var i = 0; i < names.length; ++i) {
+        console.log(names[i]);
+    }
+
+    names.forEach(function (element, index, array) {
+        console.log(element);
+    })
+
+    // Bonus problems
+
+    var meal = ["Fries, fries, and more fries", "Metric ton of sandwiches", "Your body weight in pizza", "Scavenge the local area", "Lick an apple", "A single peanut", "Zero carb water", "Photosynthesize", "Tacos", "Burgers", "Fried frish", "Nothing, you didn't pack a lunch"];
+
+
+    var whatMeal = Math.floor(Math.random() * 6) + 1;
+
+    // Make a function that will take an array value, i++, and place it in a array, while a nested on takes the new generated number, and checks if it isn't already in the array'
+
 })();
