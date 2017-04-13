8aa83bdea87f2c74b283608a7b13e96219443e84
Moses Franco <mosesf@outlook.com>
2017-04-13 15:01:00 -0500

    Completed Objects exercise, not entirely sure where I was supposed to place addendum problem, will commit later once sure.

---
+++ js/objects.js
@@ -0,0 +1,123 @@
+(function(){
+    "use strict";
+
+    /**
+     * TODO:
+     * Create person object, store it in a variable named person
+     */
+        var person = new Object();
+    /**
+     * TODO:
+     * Create firstName and lastName properties in your person object, and
+     * assign your name to them
+     */
+        person.firstName = "Moses";
+        person.lastName = "Franco";
+    /**
+     * TODO:
+     * Add a sayHello method to the person object that returns a greeting using
+     * the firstName and lastName properties.
+     * console.log the returned message to check your work
+     *
+     * Example
+     * person.sayHello() // returns "Hello from Rick Sanchez!"
+     */
+        person.sayHello = function () {
+            console.log("Hello from " + person.firstName + " " + person.lastName + "!");
+        };
+        person.sayHello();
+    /** TODO: Remember this problem from before?
+     *
+     * HEB has an offer for the shoppers that buy products amounting to more
+     * than $200. Write a JS program, using conditionals, that logs to the
+     * browser, how much does Ryan, Cameron and George need to pay. We know that
+     * Cameron bought $180, Ryan $250 and George $320. Your program will have to
+     * display a line with the name of the person, the amount before the
+     * discount, the discount, if any, and the amount after the discount.
+     *
+     * Uncomment the lines below to create an array of objects where each object
+     * represents one shopper. Use a foreach loop to iterate through the array,
+     * and console.log the relevant messages for each person
+     */
+
+    var discountRate = prompt("Set discount amount: X.XX%")
+
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
+
+    shoppers.forEach(function (shoppers) {
+        if(shoppers.amount > 200){
+            console.log(shoppers.name + " you owe: $" + shoppers.amount + ", but got a discount of: " + discountRate + "%. You now owe: $" + (shoppers.amount * (1 - discountRate)) + ".");
+        }
+        else {
+            console.log(shoppers.name + " you owe: $" + shoppers.amount +  ".");
+        }
+    })
+
+})();
+
+(function () {
+    "use strict";
+
+//
+// Create an array of objects that represent books.
+// Each book should have a title and an author property.
+// The author property should be an object with a firstName and lastName.
+// Be creative and add at least 5 books to the array
+// var books =
+
+    var books = [
+        {
+            title: "Green Gardens",
+            author: {
+                firstName: "Johnathan",
+                lastName: "Appleseed"
+            }
+        },
+        {
+            title: "Forensics & Criminology 101",
+            author: {
+                firstName: "Jack",
+                lastName: "Ripper"
+            }
+        },
+        {
+            title: "World History",
+            author: {
+                firstName: "Mars",
+                lastName: "Victors"
+            }
+        },
+        {
+            title: "Better Sleep: the definitive guide to falling asleep faster",
+            author: {
+                firstName: "Sandra",
+                lastName: "Mann"
+            }
+        },
+        {
+            title: "Lorem ipsum: quid dicis amicus?",
+            author: {
+                firstName: "Brutus",
+                lastName: "Nonulli"
+            }
+        }
+    ];
+
+// log out the books array
+    console.log(books);
+
+
+// Loop through the array of books using .forEach and print out the specified information about each one.
+// start loop here
+    books.forEach(function (books, i) {
+        console.log("Book #" + i);
+        console.log("Title: " + books.title);
+        console.log("Author: " + books.author.firstName + " " + books.author.lastName);
+        console.log("---");
+    })
+// end loop here
+})();
